92280d5be4cb7e1df4f944a507188137fee96436
XaxUser <yahya6751290@gmail.com>
2026-04-02 18:26:01 +0200

    changement

--- test_python.py
+++
@@ -1,8 +0,0 @@
-fact = "green_pepper_at_blue_crate"
-obj, location = fact.split("_at_")
-print(f"Object: {obj}, Location: {location}")
-
-obj = "green_pepper"
-fact = "green_pepper_at_blue_crate"
-final_location = fact.split(f"{obj}_at_")[1]
-print(final_location)  # doit afficher: blue_crate
